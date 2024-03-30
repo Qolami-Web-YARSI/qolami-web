a6ce757f1b119bfdeb2f70f4303fc70facdf8abf
rizalabimanyu202 <100014216+rizalabimanyu202@users.noreply.github.com>
2024-03-30 20:51:07 +0700

    revisi design code 3

--- src/components/CardContentHome2.jsx
+++ src/components/CardContentHome2.jsx
@@ -4,7 +4,7 @@ const CardContentHome2 = (props) => {
     return(
         <>
             <div class="tw-flex tw-flex-col tw-justify-between tw-bg-[#009900] tw-rounded-[30px] tw-px-4 tw-py-5 tw-mx-5">
-                <p class="tw-text-[28px] md:tw-text-[35px] xl:tw-text-[45px] 2xl:tw-text-[55px] tw-font-bold tw-text-center tw-text-[#FFF6D9] tw-my-5 tw-mx-auto">{props.judul}</p>
+                <p class="tw-text-[28px] md:tw-text-[23px] xl:tw-text-[37px] 2xl:tw-text-[45px] tw-font-bold tw-text-center tw-text-[#FFF6D9] tw-my-5 tw-mx-auto">{props.judul}</p>
                 <img src={`${props.gambar}`} class="tw-mx-auto"/>
             </div>
         </>
