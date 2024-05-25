a1afdfb3d0d0701a2058013bd0b232f2a5637da4
rizalabimanyu202 <100014216+rizalabimanyu202@users.noreply.github.com>
2024-05-25 23:18:35 +0700

    re etcetera 2.1

--- src/pages/DialogBerhasil4.jsx
+++ src/pages/DialogBerhasil4.jsx
@@ -14,7 +14,7 @@ const DialogBerhasil4 = ({id}) => {
                         <div className="modal-body tw-px-[50px] tw-rounded-[25px] tw-bg-[#FFF6D9]">
                             <div className="tw-flex tw-flex-col tw-gap-6 tw-py-5">
                                 <div className="tw-flex tw-bg-[#DBF9F9] tw-border-[3px] tw-border-[#81DEDF] tw-rounded-[23px] tw-py-10 tw-px-12">
-                                    <p className="tw-mx-auto sm:tw-text-[20px] lg:tw-text-[23px] tw-text-[#628478]">Apakah Anda yakin ingin memulai Ujian?</p>
+                                    <p className="tw-mx-auto sm:tw-text-[20px] lg:tw-text-[23px] tw-text-[#628478]">Apakah Anda yakin ingin memulai ujian?</p>
                                 </div>
                                 <div className='tw-flex tw-mx-auto tw-gap-10'>
                                     <button className="btn btn-secondary tw-text-[20px] tw-bg-[#E9ECEF] hover:tw-bg-[#E0E0E3] 
